0bbb7d8044946304f23574bb4e350d009bb209cc
Nathan Neuzil <neuziln@gmail.com>
2013-11-19 20:28:36 -0600

    Second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouponFinder::Application.config.secret_token = '402983cbc5c6a7418e318ec772869ad0a1631c46d4a2856bedb269ccdd351cd2767c5c4d49e3b39aea045ef780c488d71a8508c45808fb0e0166314e816296e9'
