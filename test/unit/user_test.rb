d62c5b303c0c033ecd3d2a18d9be7c61ce96dcc5
Nathan Neuzil <neuziln@gmail.com>
2013-12-15 00:10:30 -0600

    new tests made

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test " a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+  
+  test " a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+  
+  test " a user should enter a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+  
+  test "user shoudl have unique profile name" do
+  	user = User.new
+  	user.id = 3
+  	user.email = "neuziln@gmail.com"
+  	user.profile_name = "neuziln"
+  	user.first_name = "Nathan"
+  	user.last_name = "Neuzil"
+  	user.password = "nathan18"
+  	user.password_confirmation = "nathan18"
+  	
+  	puts user.errors.inspect
+  end
 end
